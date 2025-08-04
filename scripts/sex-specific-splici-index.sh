3eacd34f9624fd8e8be28f702c60283a2ab6ed38
Alex DeCasien <44094797+ardecasien@users.noreply.github.com>
2025-08-04 17:05:14 -0400

    Create sex-specific-splici-index.sh

---
+++ scripts/sex-specific-splici-index.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+
+# scripts/sex-specific-splici-index.sh
+# Creates sex-specific splici transcriptome indices
+
+set -e
+
+# Download 10x reference
+wget https://cf.10xgenomics.com/supp/cell-exp/refdata-gex-GRCh38-2020-A.tar.gz
+tar xzf refdata-gex-GRCh38-2020-A.tar.gz
+
+# Genome metadata
+genome="GRCh38_noY"
+version="2020-A"
+
+# Set up directories
+build="GRCh38-2020-A_build_noY"
+source="reference_sources"
+mkdir -p "$build" "$source"
+
+cd "$source"
+
+# Download reference files
+fasta_url=http://ftp.ensembl.org/pub/release-98/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz
+if [[ ! -f "Homo_sapiens.GRCh38.dna.primary_assembly.fa" ]]; then
+    wget $fasta_url
+    gunzip -c Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz > Homo_sapiens.GRCh38.dna.primary_assembly.fa
+fi
+
+gtf_url=http://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_32/gencode.v32.primary_assembly.annotation.gtf.gz
+if [[ ! -f "gencode.v32.primary_assembly.annotation.gtf" ]]; then
+    wget $gtf_url
+    gunzip -c gencode.v32.primary_assembly.annotation.gtf.gz > gencode.v32.primary_assembly.annotation.gtf
+fi
+
+cd ..
+
+fasta_in="${source}/Homo_sapiens.GRCh38.dna.primary_assembly.fa"
+gtf_in="${source}/gencode.v32.primary_assembly.annotation.gtf"
+
+# Modify FASTA headers to match GENCODE format
+fasta_modified="$build/$(basename "$fasta_in").modified"
+cat "$fasta_in" \
+    | sed -E 's/^>(\S+).*/>\1 \1/' \
+    | sed -E 's/^>([0-9]+|[XY]) />chr\1 /' \
+    | sed -E 's/^>MT />chrM /' \
+    > "$fasta_modified"
+
+# Remove version suffixes from GTF IDs
+gtf_modified="$build/$(basename "$gtf_in").modified"
+ID="(ENS(MUS)?[GTE][0-9]+)\.([0-9]+)"
+cat "$gtf_in" \
+    | sed -E 's/gene_id "'"$ID"'";/gene_id "\1"; gene_version "\3";/' \
+    | sed -E 's/transcript_id "'"$ID"'";/transcript_id "\1"; transcript_version "\3";/' \
+    | sed -E 's/exon_id "'"$ID"'";/exon_id "\1"; exon_version "\3";/' \
+    > "$gtf_modified"
+
+# Define biotype patterns
+BIOTYPE_PATTERN="(protein_coding|lncRNA|IG_C_gene|IG_D_gene|IG_J_gene|IG_LV_gene|IG_V_gene|IG_V_pseudogene|IG_J_pseudogene|IG_C_pseudogene|TR_C_gene|TR_D_gene|TR_J_gene|TR_V_gene|TR_V_pseudogene|TR_J_pseudogene)"
+GENE_PATTERN="gene_type \"${BIOTYPE_PATTERN}\""
+TX_PATTERN="transcript_type \"${BIOTYPE_PATTERN}\""
+READTHROUGH_PATTERN="tag \"readthrough_transcript\""
+PAR_PATTERN="tag \"PAR\""
+
+# Create gene allowlist
+cat "$gtf_modified" \
+    | awk '$3 == "transcript"' \
+    | grep -E "$GENE_PATTERN" \
+    | grep -E "$TX_PATTERN" \
+    | grep -Ev "$READTHROUGH_PATTERN" \
+    | grep -Ev "$PAR_PATTERN" \
+    | sed -E 's/.*(gene_id "[^"]+").*/\1/' \
+    | sort \
+    | uniq \
+    > "${build}/gene_allowlist"
+
+echo "Gene allowlist contains $(wc -l < ${build}/gene_allowlist) genes"
+
+# Filter GTF
+gtf_filtered="${build}/$(basename "$gtf_in").filtered"
+grep -E "^#" "$gtf_modified" > "$gtf_filtered"
+grep -Ff "${build}/gene_allowlist" "$gtf_modified" >> "$gtf_filtered"
+
+# Create Y chromosome mask
+module load bioawk bedtools
+bioawk -c fastx '$name=="chrY" {print $name"\t1\t"length($seq)}' "$fasta_modified" > rm_Y.bed
+
+# Mask Y chromosome for female reference
+fasta_modified_noY="$build/$(basename "$fasta_in").modified.maskedY"
+bedtools maskfasta -fi "$fasta_modified" -bed rm_Y.bed -fo "$fasta_modified_noY"
+
+# Build Cell Ranger references
+module load cellranger/6.0.0
+
+# Male reference (with Y)
+cellranger mkref \
+    --ref-version="$version" \
+    --genome="GRCh38_male" \
+    --fasta="$fasta_modified" \
+    --genes="$gtf_filtered"
+
+# Female reference (Y masked)
+cellranger mkref \
+    --ref-version="$version" \
+    --genome="$genome" \
+    --fasta="$fasta_modified_noY" \
+    --genes="$gtf_filtered"
+
+# Unzip GTF files
+gunzip GRCh38_male/genes/genes.gtf.gz
+gunzip GRCh38_noY/genes/genes.gtf.gz
+
+echo "Sex-specific references created successfully"
