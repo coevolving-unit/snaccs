5aa9b5105684bd853534cbe296571a48aa695d8e
Alex DeCasien <44094797+ardecasien@users.noreply.github.com>
2025-08-05 16:58:45 -0400

    Update and rename run_findMotifs.sh to run_findMotifs_vertebrates.sh

---
+++ scripts/run_findMotifs_vertebrates.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Add HOMER to PATH
+PATH=$PATH:/data/homer/bin/
+
+# List of clusters
+clus=(1 2 3 4 5 6 7 8 9 10 11 12 13)
+
+# Submit sbatch jobs using the vertebrates motif set
+for clus in "${clus[@]}"; do
+    sbatch --export=CLUS=$clus,INPUT_FILE="cluster${clus}-homer-mn.txt",OUTPUT_PREFIX="cluster${clus}-homer-mn",BG_FILE="cluster${clus}-homer-bgn.txt" scripts/run_findMotifs_sbatch_vertebrates.sh
+    sbatch --export=CLUS=$clus,INPUT_FILE="cluster${clus}-homer-fn.txt",OUTPUT_PREFIX="cluster${clus}-homer-fn",BG_FILE="cluster${clus}-homer-bgn.txt" scripts/run_findMotifs_sbatch_vertebrates.sh
+    sbatch --export=CLUS=$clus,INPUT_FILE="cluster${clus}-homer-sn.txt",OUTPUT_PREFIX="cluster${clus}-homer-sn",BG_FILE="cluster${clus}-homer-bgn.txt" scripts/run_findMotifs_sbatch_vertebrates.sh
+done
